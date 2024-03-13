ab98b254d1388e949dd754dfcedf017f948bee21
BrunoConceica0 <bd687353@gmail.com>
2024-03-13 17:18:29 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const vm = new Vue({
+  el: "#app",
+  data: {
+    question1: true,
+    question2: false,
+    question3: false,
+    question4: false,
+    question5: false,
+  },
+  // methods: {
+  //   questionActive() {
+  //     this.question = !this.question;
+  //   },
+  // },
+});
